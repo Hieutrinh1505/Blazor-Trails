cf9e21abadd006c86de82ad4382afdae5d0686a5
Hieu Trinh <hieutrinh358@gmail.com>
2023-02-01 22:12:38 -0600

    add Validation using FluentValidation before adding new trail

--- BlazingTrails.Shared/Features/ManageTrails/TrailDto.cs
+++ BlazingTrails.Shared/Features/ManageTrails/TrailDto.cs
@@ -1,4 +1,5 @@
-﻿namespace BlazingTrails.Shared.Features.ManageTrails;
+﻿using FluentValidation;
+namespace BlazingTrails.Shared.Features.ManageTrails;
 public class TrailDto
 {
     public int Id { get; set; }
@@ -17,4 +18,17 @@ public class TrailDto
 
 
 }
+public class TrailValidator : AbstractValidator<TrailDto>
+{
+    public TrailValidator()
+    {
+        RuleFor(x => x.Name).NotEmpty().WithMessage("Please enter a name");
+        RuleFor(x => x.Description).NotEmpty().WithMessage("Please enter a description");
+        RuleFor(x => x.Location).NotEmpty().WithMessage("Please enter a location");
+        RuleFor(x => x.Length).GreaterThan(0).WithMessage("Please enter a length");
+        RuleFor(x => x.Route).NotEmpty().WithMessage("Please enter a route instruction");
+
+    }
+}
+
 
